18b54e9bad85d7b769aac191f9c866aad4061f3d
DBDev90 <bordinassidouglas@gmail.com>
2023-03-22 11:00:37 -0300

    Atividade em sala

--- WebApp_DB/Controllers/HomeController.cs
+++ WebApp_DB/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebApp_DB.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
