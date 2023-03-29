84c5c53edbccaf4e6284606715a64f68ac441670
DBDev90 <bordinassidouglas@gmail.com>
2023-03-29 11:04:02 -0300

    Interação fronte com back

--- WebApp_DB/Controllers/ClientesController.cs
+++ WebApp_DB/Controllers/ClientesController.cs
@@ -13,5 +13,31 @@ namespace WebApp_DB.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Cadastro(string Nome, string Telefone)
+        {
+            if (string.IsNullOrEmpty(Nome))
+            {
+                TempData["Erro"] = "O campo Nome não pode estar em branco!";
+            }
+
+            if (string.IsNullOrEmpty(Telefone))
+            {
+                TempData["Erro"] = "O campo Telefone não pode estar em branco!";
+            }
+
+            return View();
+        }
+
+        public IActionResult Editar(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Excluir(int id)
+        {
+            return View();
+        }
     }
 }
