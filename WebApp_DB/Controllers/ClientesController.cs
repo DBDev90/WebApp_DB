72fddcbe46e9c9517e9779cc3c98760991907222
DBDev90 <bordinassidouglas@gmail.com>
2023-04-19 10:41:28 -0300

    Continuação do desenvolvimento

--- WebApp_DB/Controllers/ClientesController.cs
+++ WebApp_DB/Controllers/ClientesController.cs
@@ -1,17 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_DB.Models;
 
 namespace WebApp_DB.Controllers
 {
     public class ClientesController : Controller
     {
+        public static List<ClienteViewModel> lista = new List<ClienteViewModel>();
         public IActionResult Lista()
         {
-            return View();
+            //ClienteViewModel novo = new ClienteViewModel();
+            //List<ClienteViewModel> lista = new List<ClienteViewModel>();
+
+            //for (var i = 0; i < 15; i++)
+            //{                
+            //    novo.Nome = "Douglas Bordinassi";
+            //    novo.Id = 10;
+            //    novo.Telefone = "1699999 9999";
+
+            //    lista.Add(novo);
+            //}            
+            
+
+            return View(lista);
         }
 
         public IActionResult Cadastro()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SalvarDados(ClienteViewModel model)
+        {
+            lista.Add(model);
+            return RedirectToAction("Lista");
         }
 
         [HttpPost]
@@ -37,7 +59,13 @@ namespace WebApp_DB.Controllers
 
         public IActionResult Excluir(int id)
         {
-            return View();
+            ClienteViewModel cliente = lista.Find(x => x.Id == id);
+            if(cliente != null)
+            {
+                lista.Remove(cliente);
+            }
+
+            return RedirectToAction("Lista");
         }
     }
 }
